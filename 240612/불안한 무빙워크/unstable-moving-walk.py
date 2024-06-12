fce17a6e05de4249bf7c8e9521688868f87a33ad
SONG <69496202+song248@users.noreply.github.com>
2024-06-12 22:37:56 +0900

    Title: 불안한 무빙워크, Time: 5033ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 240612/불안한 무빙워크/unstable-moving-walk.py
+++ 240612/불안한 무빙워크/unstable-moving-walk.py
@@ -1,70 +1,29 @@
-n, k = tuple(map(int, input().split()))
-u = [(0, False) for _ in range(n)]
-d = [(0, False) for _ in range(n)]
+from collections import deque
 
+n, k = map(int, input().split())
+safe = list(map(int, input().split()))
 
-def shift():
-    temp = u[n - 1]
-    for i in range(n - 1, 0, -1):
-        u[i] = u[i - 1]
-    u[0] = d[n - 1]
-    for i in range(n - 1, 0, -1):
-        d[i] = d[i - 1]
-    d[0] = temp
+belt = deque([])
+for i in safe:
+    belt.append([i, 0])
 
-def can_go(idx):
-    if idx == n:
-        return True
-    stability, occupied = u[idx]
-    return stability > 0 and not occupied
-
-def move(idx):
-    curr_stability, _ = u[idx]
-    u[idx] = (curr_stability, False)
-    if idx + 1 < n:
-        next_stability, _ = u[idx + 1]
-        u[idx + 1] = (next_stability - 1, True)
-
-def move_all():
-    for i in range(n - 1, -1, -1):
-        _, occupied = u[i]
-        if occupied and can_go(i + 1):
-            move(i)
-
-def add():
-    stability, occupied = u[0]
-    if stability > 0 and not occupied:
-        u[0] = (stability - 1, True)
-
-def simulate():
-    shift()
-    move_all()
-    add()
-    _, occupied = u[n - 1]
-    if occupied:
-        move(n - 1)
-
-def done():
-    unstable_cnt = 0
-    for stability, _ in u:
-        if not stability:
-            unstable_cnt += 1
-
-    for stability, _ in d:
-        if not stability:
-            unstable_cnt += 1
-    
-    return unstable_cnt >= k
-
-given_row = list(map(int, input().split()))
-for i, stability in enumerate(given_row[:n]):
-    u[i] = (stability, False)
-for i, stability in enumerate(given_row[n:]):
-    d[i] = (stability, False)
-
-trial = 0
-while not done():
-    simulate()
-    trial += 1
-
-print(trial)
+cnt = 0
+while True:
+    k_list = list(map(lambda x: x[0], belt))      
+    if k_list.count(0) == k:
+        break
+    tmp = belt.pop()
+    belt.appendleft(tmp)
+    for i in range(n-1):
+        if belt[i][1] == 1:
+            if belt[i+1][0] != 0 and belt[i+1][1] == 0:
+                belt[i][1] = 0
+                belt[i+1][1] = 1 
+                belt[i+1][0] -= 1
+    if belt[n-1][1] >= 1:
+        belt[n-1][1] = 0
+    if belt[0][1] == 0:
+        belt[0][1] += 1
+        belt[0][0] -= 1
+    cnt += 1
+print(cnt)
