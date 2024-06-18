564ffc9395a21f2c91688ef2fc52e2bbd53847c3
SONG <69496202+song248@users.noreply.github.com>
2024-06-18 11:59:43 +0900

    Title: 이상한 진수, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240618/이상한 진수/awkward-digits.py
+++ 240618/이상한 진수/awkward-digits.py
@@ -1,14 +1,16 @@
-binary = list(map(int, input()))
-length = len(binary)
+a = str(input())
+b = str(input())
 
-INT_MIN = -9999999
-ans = INT_MIN
-for i in range(length):
-    binary[i] = 1-binary[i]
+a_list = set()
+b_list = set()
 
-    num = 0
-    for j in range(length):
-        num += (2**(length-j-1)) * binary[j]
-    ans = max(ans, num)
-    binary[i] = 1-binary[i]
-print(ans)
+for i in range(len(a)):
+    for bit in [0, 1]:
+        new_a = a[:i] + str(bit) + a[i+1:]
+        a_list.add(int(new_a, 2))
+
+for i in range(len(b)):
+    for bit in [0, 1, 2]:
+        new_b = b[:i] + str(bit) + b[i+1:]
+        b_list.add(int(new_b, 3))
+print(list(a_list.intersection(b_list))[0])
